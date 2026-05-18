09d19031419c3f58838482d3e27c6ec352814f9b
Minhmarks <minhnhat21012005@at21012005gmail.com>
2026-05-18 23:25:06 +0700

    delete some completed dataste

--- DataProcessing/generate_oc_datasets.py
+++ DataProcessing/generate_oc_datasets.py
@@ -58,9 +58,9 @@ DATASETS = [
     # ('N_BaIoT', N_BaIoT, 'class', ['benign', 'Benign', 0, '0']),
     # ('BoTIoT', BoTIoT, 'subcategory', ['Normal', 'normal', 0, '0']), 
     # ('CICIoT2023', CICIoT2023, 'label', ['BenignTraffic', 'Benign', 'normal', 'Normal', 0, '0']),
-    ('EdgeIIoTset', EdgeIIoTset, 'label', ['Normal', 'normal', 0, '0']),
+    # ('EdgeIIoTset', EdgeIIoTset, 'label', ['Normal', 'normal', 0, '0']),
     ('FiveGNIDD', FiveGNIDD, 'label', ['Normal', 'normal', 0, '0']),
-    ('IoTID20', IoTID20, 'label', ['Normal', 'normal', 0, '0'])
+    # ('IoTID20', IoTID20, 'label', ['Normal', 'normal', 0, '0'])
 ]
 
 def generate_datasets():
